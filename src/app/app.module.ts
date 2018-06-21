58e124eeb335b341f0de9702a36bd68c6d7114fa
Marcus Finnberg <marcus.finnberg@outlook.com>
2018-06-21 12:06:13 +0200

    added the initial components and services needed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { TeamComponent } from './team/team.component';
+import { SocialComponent } from './social/social.component';
+import { ChatComponent } from './chat/chat.component';
+import { ContactComponent } from './contact/contact.component';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    CalendarComponent,
+    TeamComponent,
+    SocialComponent,
+    ChatComponent,
+    ContactComponent,
+    AdminComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
